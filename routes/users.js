8f3809bbef8187b24b013d2a675bda551db4be0c
sahandbabali <sahand.babali@gmail.com>
2021-05-27 18:32:02 +0430

    after saving users in database and hashing passwords

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
 
 // /api/users - creates new user - Public
 router.post(
@@ -13,12 +14,33 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("passed");
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send("User added successfully");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
