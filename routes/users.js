a9f571e8227f71bb415692c32c38f11c36959d5e
sahandbabali <sahand.babali@gmail.com>
2021-05-27 18:51:26 +0430

    after creating jwt and sending back to user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
+const config = require("config");
 
 // /api/users - creates new user - Public
 router.post(
@@ -27,16 +29,38 @@ router.post(
       if (user) {
         return res.status(400).json({ msg: "User already exists" });
       }
+
       user = new User({
         name,
         email,
         password,
       });
-
+      // hashing password
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
+
       await user.save();
-      res.send("User added successfully");
+
+      // jsonwebtoken
+      const jwtpayload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        jwtpayload,
+        config.get("jwtsecret"),
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) {
+            throw err;
+          } else {
+            res.json({ token });
+          }
+        }
+      );
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server error");
